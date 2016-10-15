db72aa0591ff9f4aa37169d14a77bf3b3d0c47a0
James Sena <jamesson.sena2009@hotmail.com>
2016-10-15 18:00:49 -0300

    Primeiro commit utilizando a IDE Android Studio

---
+++ app/src/main/java/br/sp/senai/imformatica/afazer/model/Item.java
@@ -0,0 +1,26 @@
+package br.sp.senai.imformatica.afazer.model;
+
+/**
+ * Created by Android1 on 15/10/2016.
+ */
+
+public class Item{
+    private Long id;
+    private String titulo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
---
+++ app/src/main/java/br/sp/senai/imformatica/afazer/model/ItemDAO.java
@@ -0,0 +1,28 @@
+package br.sp.senai.imformatica.afazer.model;
+
+/**
+ * Created by Android1 on 15/10/2016.
+ */
+
+public class ItemDAO {
+    private List<Item> lista;
+    private static long id = 0;
+
+    public void salvar(Item item){
+
+    }
+
+    public Item localizar(long id){
+        return null;
+    }
+
+
+
+    public List<Item> listar(){
+        return null;
+    }
+
+    public void remover(long id){
+
+    }
+}
